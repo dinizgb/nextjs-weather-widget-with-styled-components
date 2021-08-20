ed43c56def6463f7d153975e43793a1bd69a61d8
gabrielsdinizbrq <gabrielsdiniz@brq.com>
2021-08-20 16:51:41 -0300

    Weather Widget V1

---
+++ components/WeatherWidget_LatLon.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import Image from 'next/image';
+import styled from 'styled-components';
+
+const WeatherWidgetWrapper = styled.div`
+    min-width: 250px;
+    background: #fff;
+    border: 1px solid #e8eaff;
+    border-radius: 6px;
+    padding: 15px 15px 0 15px;
+    margin: 20px;
+`
+
+const WidgetTitle = styled.h1`
+    font-size: 16px;
+    font-weight: bold;
+    color: #666;
+`
+
+const IconArea = styled.div`
+    padding: 0 5px;
+`
+
+const TempText = styled.div`
+    font-size: 30px;
+    font-weight: bold;
+    color: #666;
+    margin-top: 24px;
+`
+
+export default function WeatherWidget_LatLon() {
+    const [weather, setWeather] = useState({});
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                fetchWeather(position.coords.latitude, position.coords.longitude);
+            });
+            const fetchWeather = async (latitude, longitude) => {
+                try{
+                    const weatherReq = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=ee94006c9bb74ce892f181126211305&q=${latitude},${longitude}&days=1&aqi=no&alerts=no`);
+                    const weatherData = await weatherReq.json();
+                    setWeather({icon: <Image src={"https:" + weatherData.current.condition.icon} alt={`It is ${weatherData.current.temp_c} in your city`} width={80} height={80} />, temperature: weatherData.current.temp_c + "º C"});
+                }
+                catch{
+                    console.log("xWthErr");
+                }
+            };
+        }
+    }, []);
+    return (
+        <WeatherWidgetWrapper>  
+            <div className="row">
+                <WidgetTitle>With Latitude and Longitude</WidgetTitle>
+            </div>
+            <div className="row">
+                <IconArea>{weather.icon}</IconArea>
+                <TempText>{weather.temperature}</TempText>
+            </div>
+        </WeatherWidgetWrapper>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,56 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  html, body {
+    background: #f5f6fe;
+    font-family: 'Roboto', sans-serif;
     box-sizing: border-box;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 30px;
+    color: #000;
+  }
+  main {
+    min-height: calc(100vh - 125px);
+  }
+  *{
+    margin: 0;
+    -webkit-transition: 0.25s;
+    -moz-transition: 0.25s;
+    -o-transition: 0.25s;
+    -ms-transition: 0.25s;
+    transition: 0.25s;
+    &:hover {
+      -webkit-transition: 0.2s;
+      -moz-transition: 0.2s;
+      -o-transition: 0.2s;
+      -ms-transition: 0.2s;
+      transition: 0.2s;
+    }
+  }
+  .container {
+    max-width: 1024px;
+    padding: 0 6rem;
+    margin: 0 auto;
+    height: 100%;
+    position: relative;
+    @media (max-width: 1100px) {
+      padding: 0;
+    }
+  }
+  .row{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .space-between{
+    justify-content: space-between;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: '#000',
   },
 }
 
